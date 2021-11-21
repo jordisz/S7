cae53bb73fdc6497a63c452a6db5ec4b7950a2da
Jordi Sánchez <60137213+jordisz@users.noreply.github.com>
2021-11-21 12:21:31 +0100

    Add QuantityInput component and Panell animation

--- src/components/Panell.vue
+++ src/components/Panell.vue
@@ -2,35 +2,37 @@
   <div class="panell">
       <div class="row">
           <label>Número de pàgines</label>
-          <input type="text" 
-            @keypress="filtraNumeros" 
-            @keyup="$emit('paginesChanged', this.pagines)" 
-            v-model="pagines">
+          <QuantityInput v-on:quantityChanged="changePagines"/>
       </div>
       <div class="row">
           <label>Número d'idiomes</label>
-          <input type="text" 
-            @keypress="filtraNumeros" 
-            @keyup="$emit('idiomesChanged', this.idiomes)" 
-            v-model="idiomes">
+          <QuantityInput v-on:quantityChanged="changeIdiomes"/>
       </div>
   </div>
 </template>
 
 <script>
+import QuantityInput from './QuantityInput.vue'
+
 export default {
     name: "Panell",
+    components: {
+        QuantityInput
+    },
     data() {
         return {
-            idiomes: '',
-            pagines: ''
+            idiomes: 1,
+            pagines: 1
         }
     },
     methods: {
-        filtraNumeros(e) {
-            if(e.keyCode < 48 || e.keyCode > 57) {
-                e.preventDefault();
-            }
+        changePagines(val) {
+            this.pagines = val;
+            this.$emit('paginesChanged', this.pagines);
+        },
+        changeIdiomes(val) {
+            this.idiomes = val;
+            this.$emit('idiomesChanged', this.idiomes);
         }
     }
 }
@@ -38,15 +40,16 @@ export default {
 
 <style>
     .panell {
-        width: 320px;
-        border: 3px solid black;
+        width: 250px;
+        border: 2px solid black;
         border-radius: 15px;
         padding: 2em 1.5em;
-        margin: 1em 0;
+        margin: 1em 0 1em .5em;
     }
     .row {
         display: flex;
         justify-content: space-between;
+        align-items: center;
     }
     .row:first-of-type {
         margin-bottom: 1em;
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,35 @@
 <template>
   <div>
     <p>Què vols fer?</p>
+  
     <div class="checkbox">
       <input type="checkbox" v-model="web" @click="resetPanell">
       <label>Una pàgina web (500€)</label>
     </div>
-    <Panell
-      v-if="web" 
-      v-on:paginesChanged="addPagines" 
-      v-on:idiomesChanged="addIdiomes"/>
+
+    <transition name="grow">
+      <Panell
+        v-if="web"
+        v-on:paginesChanged="addPagines"
+        v-on:idiomesChanged="addIdiomes"/>
+    </transition>
+    
     <div class="checkbox">
       <input type="checkbox" v-model="seo">
       <label>Una consultoria SEO (300€)</label>
     </div>
+    
     <div class="checkbox">
       <input type="checkbox" v-model="ads">
       <label>Una campanya de Google Ads (200€)</label>
     </div>
+    
     <p>Preu: {{ preu }} €</p>
+  
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import Panell from '@/components/Panell.vue'
 
 export default {
@@ -35,8 +42,8 @@ export default {
       web: false,
       seo: false,
       ads: false,
-      pagines: 0,
-      idiomes: 0
+      pagines: 1,
+      idiomes: 1
     }
   },
   computed: {
@@ -54,20 +61,24 @@ export default {
       if (this.pagines > 0 && this.idiomes > 0) {
         total += (this.pagines * this.idiomes * 30)
       }
-
       return total;
     }
   },
   methods: {
-    addPagines(str) {
-      this.pagines = +str;
+    addPagines(val) {
+      this.pagines = +val;
     },
-    addIdiomes(str) {
-      this.idiomes = +str;
+    addIdiomes(val) {
+      this.idiomes = +val;
     },
     resetPanell() {
-      this.pagines = 0;
-      this.idiomes = 0;
+      if(!this.web) {
+        this.pagines = 1;
+        this.idiomes = 1;
+      } else {
+        this.pagines = 0;
+        this.idiomes = 0;
+      }
     }
   }
 }
@@ -77,4 +88,22 @@ export default {
   .checkbox {
     margin-bottom: 1em;
   }
+
+  .grow-enter-active {
+    transform-origin: top left;
+    animation: grow .6s ease-in-out;
+  }
+  .grow-leave-active {
+    transform-origin: top left;
+    animation: grow .3s reverse ease-in-out;
+  }
+  @keyframes grow {
+    0% {
+      transform: scale(0);
+    }
+    100% {
+      transform: scale(1);
+    }
+  }
+
 </style>
